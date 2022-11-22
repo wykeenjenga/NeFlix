a144d2e59363f0aa458c4b12506c1c02ec74107f
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-11-22 23:45:48 +0300

    Initial Commit

---
+++ NeatFlix/NeatFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeatFlixApp.swift
+//  NeatFlix
+//
+//  Created by Wykee on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NeatFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
